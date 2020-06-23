4238b5096d786ebcbc2a004da46486e8c439cb6c
Yozgyan <yozgyanshakir93@gmail.com>
2020-06-23 17:34:27 +0300

    Add docs folder

---
+++ docs/precache-manifest.b580a79cf753eab2d8430e0577c1be9b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32049c787e477c48e8c4805a1ecff70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b140c774d0c3571583cd",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "cf9bfe753ae9f1bed1ff",
+    "url": "/static/css/main.a06a617e.chunk.css"
+  },
+  {
+    "revision": "b140c774d0c3571583cd",
+    "url": "/static/js/2.02349c50.chunk.js"
+  },
+  {
+    "revision": "cf9bfe753ae9f1bed1ff",
+    "url": "/static/js/main.bbf9d6d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2198ae58b4adb476f1a11bcda1209e9a",
+    "url": "/static/media/TCG.2198ae58.png"
+  },
+  {
+    "revision": "880a8ad360ab3ac414390603b48be461",
+    "url": "/static/media/XPO.880a8ad3.png"
+  },
+  {
+    "revision": "6416af0cc6b06e4972c1f90e9c34e0ba",
+    "url": "/static/media/cv.6416af0c.jpg"
+  }
+]);
